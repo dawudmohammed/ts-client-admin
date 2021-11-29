e43c8ff7503d8744f76964f69e7e70d7ccc06efd
geraldelorm <geraldelorm14@gmail.com>
2021-11-29 12:00:29 +0000

    initial commit

---
+++ src/main/java/io/turntabl/tsclientadmin/TsClientAdminApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.tsclientadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsClientAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsClientAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/tsclientadmin/TsClientAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.tsclientadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsClientAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
